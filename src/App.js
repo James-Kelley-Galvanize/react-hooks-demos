bdfb4eca6f6126d524f9deec4aa1f619a2d8c6a6
James-Kelley-Galvanize <james.kelley@galvanize.com>
2021-06-03 15:28:13 -0600

    Build out useReducer demo with buttons and counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import "./App.css";
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialState = { count: 0 };
+
+function countReducer(state, action) {
+	const { count } = state;
+	switch (action.type) {
+		case `increment`:
+			return { count: count + 1 };
+		case `decrement`:
+			return { count: count - 1 };
+		case `times2`:
+			return { count: count * 2 };
+		case `times3`:
+			return { count: count * 3 };
+		case `divide2`:
+			return { count: count / 2 };
+		case `zero`:
+			return { count: 0 };
+		default:
+			throw new Error();
+	}
+}
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [state, dispatch] = useReducer(countReducer, initialState);
+	const { count } = state;
+	const prettyCount = count.toFixed(2); // Make number always have 2 digits after it when displayed on page
 	return (
 		<div className="App">
-			<h1>useState Hook Demo</h1>
-			<h1>THE COUNT IS: {count}</h1>
-			<button
-				onClick={() => {
-					setCount(count + 1);
-				}}
-			>
-				+
-			</button>
-			<button
-				onClick={() => {
-					setCount(count - 1);
-				}}
-			>
-				-
-			</button>
+			<h1>useReducer Hook Demo</h1>
+			<h1>THE COUNT IS: {prettyCount}</h1>
+			<button onClick={() => dispatch({ type: `increment` })}>➕1️⃣</button>
+			<button onClick={() => dispatch({ type: `decrement` })}>➖1️⃣</button>
+			<button onClick={() => dispatch({ type: `times2` })}>✖️2️⃣</button>
+			<button onClick={() => dispatch({ type: `times3` })}>✖️3️⃣</button>
+			<button onClick={() => dispatch({ type: `divide2` })}>➗2️⃣</button>
+			<button onClick={() => dispatch({ type: `zero` })}> 🔄 </button>
 		</div>
 	);
 }
