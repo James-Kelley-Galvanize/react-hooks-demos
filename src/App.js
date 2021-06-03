280457965a49164c5655117656a63722a1e1c43f
James-Kelley-Galvanize <james.kelley@galvanize.com>
2021-06-03 15:07:13 -0600

    Adjust style, name hook in heading

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
 	const [count, setCount] = useState(0);
 	return (
 		<div className="App">
+			<h1>useState Hook Demo</h1>
 			<h1>THE COUNT IS: {count}</h1>
 			<button
 				onClick={() => {
